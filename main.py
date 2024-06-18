2e65b202d16c5cc8e5b75c83757bcda7c9d734d2
S3B4ST14N909 <168067780+S3B4ST14N909@users.noreply.github.com>
2024-06-18 14:08:12 +0200

    wip: update main

--- main.py
+++ main.py
@@ -1,66 +1,20 @@
-# name: str = input('Enter your name: ')
-# print(f'WITAJ {name}')
-data_of_users: list = [
-    {'name': 'Julia', 'surname': 'Szklarzewska', 'posts': 5, 'location': 'Hajnówka'},
-    {'name': 'Norbert', 'surname': 'Szeliga', 'posts': 15, 'location': 'Rzeszów'},
-    {'name': 'Kacper', 'surname': 'Wójcik', 'posts': 20, 'location': 'Legnica'},
-    {'name': 'Sebastian', 'surname': 'Dudek', 'posts': 100, 'location': 'Siedlce'},
-]
-print(f'Witaj {data_of_users[0]["name"]}')
+from CRUD import create_user,  read_db, remove_user_db, update_db, create_user, db_params
 
 
-def read(users: list) -> None:
-    """
-    show users from a list
-    :param users: a list of users
-    :return: None
-    """
-    for user in users[1:]:
-        print(f'twój znajomy:  {user['name']}, opublikował: {user['posts']} postów')
 
 
-# read(data_of_users)
-
-def add_user(users: list) -> None:
-    """
-    add a user to a users list
-    :param users: 
-    :return: 
-    """""
-
-    name: str = input('Enter your name:')
-    surname: str = input('Enter your surname:')
-    posts: int = int(input('Enter number of your posts:'))
-    location: str = input('Enter your location:')
-    new_user: dict = {'name': name, 'surname': surname, 'posts': posts, 'location': location}
-    users.append(new_user)
+while True:
+    print('menu:')
+    print('0 ; zakończ pracę')
+    print('1 ; dodaj użytkownika')
+    print('2 ; zaktualizuj dane użytkownika')
+    print('3 ; usuń użytkownika')
+    print('4 ; wyświetl wszystkich użytkowników')
 
 
-# add_user(data_of_users)
-#
-def delete_user(users: list) -> None:
-    name: str = input('Enter a name of the user to remove:')
-    for user in users:
-        if user['name'] == name:
-            users.remove(user)
-
-
-#     read(data_of_users)
-#
-# delete_user(data_of_users)
-def update_user(users: list) -> None:
-    name: str = input("Enter a name of the user to update: ")
-    for user in users:
-        if user['name'] == name:
-            new_name: str = input('Enter a new name of the user:')
-            user['name'] = new_name
-            new_surname: str = input('Enter a new surname of the user:')
-            user['surname'] = new_surname
-            new_posts: int = int(input('Enter a new number of posts of the user:'))
-            user['posts'] = new_posts
-            new_location: str = input('Enter a new location of the user:')
-            user['location'] = new_location
-
-update_user(data_of_users)
-read(data_of_users)
-
+    menu_option: str = input('Podaj opcję do uruchomienia: ')
+    if menu_option == '0': break
+    if menu_option == '1': create_user(db_params)
+    if menu_option == '2': update_db(db_params)
+    if menu_option == '3': remove_user_db(db_params)
+    if menu_option == '4': read_db(db_params)
